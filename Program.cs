4e8326c1f023a00620bd30ef04c93114f4732848
sks <ssurovneva@mail.ru>
2023-11-28 00:39:55 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using WeaponLib;
+using static System.Console;
+
+
+
+namespace Weapon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            WriteLine("Welcome to shooter simulator");
+
+            Target t = new Target();
+            t.setStrength();
+            t.showStrength();
+
+            IWeaponFactory factory = new ChooseWeapon();
+            var item = factory.Create();
+
+            switch (item)
+            {
+                case Pistol:
+                    while (t.Strength > 0)
+                    {
+                        t.showStrength();
+                        item.Shoot();
+                        t.Strength -= 20;
+                        count++;
+                    }
+                    break;
+                case Rifle:
+                    while (t.Strength > 0)
+                    {
+                        t.showStrength();
+                        item.Shoot();
+                        t.Strength -= 50;
+                        count++;
+                    }
+                    break;
+                case Shotgun:
+                    while (t.Strength > 0)
+                    {
+                        t.showStrength();
+                        item.Shoot();
+                        t.Strength -= 80;
+                        count++;
+                    }
+                    break;
+                default: 
+                    throw new Exception ("Non-existing weapon");
+                    
+            }
+            WriteLine("The target has been defeated. Amount of shoots: " + count);
+            
+
+
+        }
+    }
+}
---
+++ Target.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weapon
+{
+    internal class Target
+    {
+        Random rand = new Random();
+        
+
+        public int Strength { get; set; }
+
+        public void setStrength()
+        {
+            Strength = rand.Next(50, 250);
+        }
+
+        public void showStrength()
+        {
+           Console.WriteLine($"Target strength: {Strength}");
+        }
+    }
+}
